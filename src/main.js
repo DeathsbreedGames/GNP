ec1704fb3a00e2e0fe277f977ee84f859f7f7c1c
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-12-17 18:26:33 -0600

    Added image to the game.

---
+++ src/main.js
@@ -0,0 +1,26 @@
+/**
+ * GNP
+ * Copyright (C) DeathsbreedGames 2014
+ * License: GNU Affero GPLv3
+ * Year 2015
+ * 
+ */
+var game = new Phaser.Game(480, 320, Phaser.AUTO, 'game', { preload:preload, create:create, update:update });
+
+// Preloader:
+function preload() {
+	game.load.image('bg', 'assets/background_glowy.png');
+	game.load.image('ball', 'assets/ball_glowy.png');
+	game.load.image('gnp', 'assets/logo.png');
+	game.load.image('paddle', 'assets/player_glowy.png');
+}
+
+// Start:
+function create() {
+	game.add.sprite(0, 0, 'bg');
+}
+
+// Update:
+function update() {
+
+}

fe9c2ef560e05b45d57534b02c4e7fa0b2970fe0
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-12-20 16:20:02 -0600

    Load audio first.

--- src/Preload.js
+++ src/Preload.js
@@ -22,13 +22,13 @@ DeathsbreedGames.Preload.prototype = {
 		this.load.setPreloadSprite(this.preloadBar);
 
 		// Load the game assets
+		this.load.audio('hit', 'assets/sfx/hitsfx.ogg');
+		this.load.audio('score', 'assets/sfx/scoresfx.ogg');
+		this.load.audio('music', 'assets/sfx/Never_Stop_Running.ogg');
 		this.load.image('bg', 'assets/gfx/background_glowy.png');
 		this.load.image('ball', 'assets/gfx/ball_glowy.png');
 		this.load.image('logo', 'assets/gfx/logo.png');
 		this.load.image('paddle', 'assets/gfx/player_glowy.png');
-		this.load.audio('hit', 'assets/sfx/hitsfx.ogg');
-		this.load.audio('score', 'assets/sfx/scoresfx.ogg');
-		this.load.audio('music', 'assets/sfx/Never_Stop_Running.ogg');
 	},
 	create:function() {
 		this.state.start('MainMenu');
